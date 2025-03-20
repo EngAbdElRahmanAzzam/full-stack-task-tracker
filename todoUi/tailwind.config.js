275c2e980f8fec02d9dec3e54beab03944b928af
Eng_AbdElRahmanAzzam <122513601+EngAbdElRahmanAzzam@users.noreply.github.com>
2025-03-20 13:32:39 +0200

    set up Meteors

--- todoUi/tailwind.config.js
+++ todoUi/tailwind.config.js
@@ -6,15 +6,24 @@ export default {
   theme: {
     extend: {
       animation: {
+        meteor: "meteor 0.2s linear infinite",
         scaleUp: "scale-up4 1s linear infinite",  
       },
       keyframes: {
         "scale-up4": {  
           "20%": { transform: "scaleY(1.5)", backgroundColor: "#3f3c3c" },  
           "40%": { transform: "scaleY(1)" }, 
-        }
+        },
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "0" },
+          "70%": { opacity: "100%" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "",
+          },
       }
     }
   },
   plugins: [],
-};
+  }
+}
